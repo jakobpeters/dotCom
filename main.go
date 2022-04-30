29733fc3971e13d6afc01546f6a10f93757f272b
Jakob <me@jakobpeters.com>
2022-04-29 21:41:14 -0700

    updated redirect

--- main.go
+++ main.go
@@ -25,15 +25,13 @@ func main() {
 	}
 }
 
-func redirect(w http.ResponseWriter, req *http.Request) {
-	// remove/add not default ports from req.Host
-	target := "https://" + req.Host + req.URL.Path
-	if len(req.URL.RawQuery) > 0 {
-		target += "?" + req.URL.RawQuery
+func redirect(w http.ResponseWriter, r *http.Request) {
+	target := "https://" + r.Host + r.URL.Path
+	if len(r.URL.RawQuery) > 0 {
+		target += "?" + r.URL.RawQuery
 	}
-	log.Printf("redirect to: %s", target)
-	http.Redirect(w, req, target,
-		// see comments below and consider the codes 308, 302, or 301
+	log.Print(target)
+	http.Redirect(w, r, target,
 		http.StatusTemporaryRedirect)
 }
 func index(w http.ResponseWriter, r *http.Request) {
