829249331519a1cc922082c7d556b966f46d0a43
Jakob <me@jakobpeters.com>
2022-05-02 19:50:09 -0700

    update to ttest digital ocean app

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 )
 
@@ -17,24 +16,29 @@ func init() {
 	}
 }
 func main() {
-	go http.ListenAndServe(":80", http.HandlerFunc(redirect))
+	//go http.ListenAndServe(":80", http.HandlerFunc(redirect))
+	//err := http.ListenAndServeTLS(":443", "/usr/local/etc/letsencrypt/live/www.jakobpeters.com/fullchain.pem", "/usr/local/etc/letsencrypt/live/www.jakobpeters.com/privkey.pem", nil)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+
 	http.HandleFunc("/", index)
-	err := http.ListenAndServeTLS(":443", "/usr/local/etc/letsencrypt/live/www.jakobpeters.com/fullchain.pem", "/usr/local/etc/letsencrypt/live/www.jakobpeters.com/privkey.pem", nil)
+
+	err := http.ListenAndServe(":7777", nil)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Print(err)
 	}
-
 }
 
-func redirect(w http.ResponseWriter, r *http.Request) {
-	target := "https://" + r.Host + r.URL.Path
-	if len(r.URL.RawQuery) > 0 {
-		target += "?" + r.URL.RawQuery
-	}
-	log.Print(target)
-	http.Redirect(w, r, target,
-		http.StatusTemporaryRedirect)
-}
+//func redirect(w http.ResponseWriter, r *http.Request) {
+//	target := "https://" + r.Host + r.URL.Path
+//	if len(r.URL.RawQuery) > 0 {
+//		target += "?" + r.URL.RawQuery
+//	}
+//	log.Print(target)
+//	http.Redirect(w, r, target,
+//		http.StatusTemporaryRedirect)
+//}
 func index(w http.ResponseWriter, r *http.Request) {
 	err = Tpl.ExecuteTemplate(w, "index.html", nil)
 	if err != nil {
